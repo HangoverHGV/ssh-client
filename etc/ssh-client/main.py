279a4849ba4c31f65fdf5860500e5abe0345a5a3
hangoverhgv <hangobogdan@@gmail.com>
2025-03-12 22:14:54 +0200

    Update icon path to use application directory for improved portability

--- etc/ssh-client/main.py
+++ etc/ssh-client/main.py
@@ -5,7 +5,8 @@ import os
 def main():
     app = wx.App(False)
     frame = App(None, title="SSH-Manager")
-    icon_path = os.path.join(os.getcwd(), 'icon.png')
+    app_dir = os.path.dirname(os.path.abspath(__file__))
+    icon_path = os.path.join(app_dir, 'icon.png')
     icon = wx.Icon(icon_path, wx.BITMAP_TYPE_PNG)
     frame.SetIcon(icon)
     frame.Show()
